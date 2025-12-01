911cbca2202fa0f0ec67f6b5ef12b6f49a1c67fc
Ethan Zhang <ethanz4@illinois.edu>
2025-12-01 13:37:20 -0600

    part 1

--- 01/day01.ml
+++ 01/day01.ml
@@ -1,14 +1,44 @@
 open! Core
 
-let p1 _input = assert false
+type direction = Left | Right
+
+let direction_of_string s =
+  match s with "L" -> Left | "R" -> Right | _ -> failwith "Unknown direction!"
+
+type rotation = { dir : direction; amt : int }
+type state = { cnt : int; curr : int }
+
+let rotation_of_string s : rotation =
+  let hd = String.prefix s 1 in
+  let tl = String.chop_prefix_exn s ~prefix:hd in
+  { dir = direction_of_string hd; amt = int_of_string tl }
+
+let p1 input =
+  let rotations = String.split_lines input |> List.map ~f:rotation_of_string in
+  let { cnt; _ } =
+    List.fold rotations ~init:{ cnt = 0; curr = 50 }
+      ~f:(fun { cnt; curr } { dir; amt } ->
+        let new_ =
+          (match dir with Left -> curr - amt | Right -> curr + amt) mod 100
+        in
+        { cnt = (if new_ = 0 then cnt + 1 else cnt); curr = new_ })
+  in
+  cnt
+
+let p2 _input = assert false
 
 let run ~part input =
   match part with
   | 1 -> p1 input
-  | 2 -> assert false
+  | 2 -> p2 input
   | _ -> failwith (sprintf "Unknow part: %d" part)
 
 let%expect_test "part 1 sample input" =
   let input = In_channel.read_all "../inputs/d1p1.sample" in
   printf "%d" (p1 input);
-  [%expect]
+  [%expect {| 3 |}]
+
+let%expect_test "part 1" =
+  let input = In_channel.read_all "../inputs/d1.in" in
+  printf "%d" (p1 input);
+  [%expect {| 1097 |}]
