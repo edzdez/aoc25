412e6d02aedf011cdf34281a8963674bd070e325
Ethan Zhang <ethanz4@illinois.edu>
2025-12-03 01:43:15 -0600

    part 1

--- 03/day03.ml
+++ 03/day03.ml
@@ -1,6 +1,30 @@
 open! Core
 
-let p1 _input = assert false
+type bank = int list
+
+let max_joltage bank =
+  (* we can be greedy *)
+  let aux =
+    let open Option.Let_syntax in
+    let%bind first =
+      List.max_elt ~compare:Int.compare @@ List.drop_last_exn bank
+    in
+    let%bind second =
+      List.max_elt ~compare:Int.compare
+      @@ List.drop (List.drop_while bank ~f:(fun x -> x <> first)) 1
+    in
+    return ((first * 10) + second)
+  in
+  match aux with Some n -> n | None -> assert false
+
+let p1 input =
+  let banks =
+    String.split_lines input
+    |> List.map ~f:(fun line ->
+        String.to_list line |> List.map ~f:Char.get_digit_exn)
+  in
+  List.fold banks ~init:0 ~f:(fun acc bank -> acc + max_joltage bank)
+
 let p2 _input = assert false
 
 let run ~part input =
@@ -16,7 +40,7 @@ let%expect_test "part 1 sample input" =
     |> String.strip ~drop:Char.is_whitespace
   in
   printf "%d" (p1 input);
-  [%expect {| 1227775554 |}]
+  [%expect {| 357 |}]
 
 (*
 let%expect_test "part 2 sample input" =
@@ -26,6 +50,7 @@ let%expect_test "part 2 sample input" =
   in
   printf "%d" (p2 input);
   [%expect {| 4174379243 |}]
+*)
 
 let%expect_test "part 1" =
   let input =
@@ -33,8 +58,9 @@ let%expect_test "part 1" =
     |> String.strip ~drop:Char.is_whitespace
   in
   printf "%d" (p1 input);
-  [%expect {| 28846518423 |}]
+  [%expect {| 17278 |}]
 
+(*
 let%expect_test "part 2" =
   let input =
     In_channel.read_all "../inputs/d3.in"
