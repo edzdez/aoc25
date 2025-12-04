73ed435f32e128e3cd27a1342c83a7fb3f5cdf5b
Ethan Zhang <ethanz4@illinois.edu>
2025-12-03 23:12:31 -0600

    part 1 (imperative :yuck:)

--- 04/day04.ml
+++ 04/day04.ml
@@ -1,6 +1,38 @@
 open! Core
 
-let p1 _input = assert false
+let in_grid ~rows ~cols (x, y) = x >= 0 && x < cols && y >= 0 && y < rows
+
+let count_neighbors ~grid ~rows ~cols (x, y) =
+  let neighbors = ref 0 in
+  for dx = -1 to 1 do
+    for dy = -1 to 1 do
+      let ((nx, ny) as p) = (x + dx, y + dy) in
+      if (dx <> 0 || dy <> 0) && in_grid ~rows ~cols p && grid.(ny).(nx) then
+        neighbors := !neighbors + 1
+    done
+  done;
+  !neighbors
+
+let p1 input =
+  let grid =
+    String.split_lines input
+    |> List.map ~f:(fun line ->
+        String.to_list line
+        |> List.map ~f:(fun c -> Char.(c = '@'))
+        |> List.to_array)
+    |> List.to_array
+  in
+  let rows = Array.length grid in
+  let cols = Array.length grid.(0) in
+  let ans = ref 0 in
+  for row = 0 to rows - 1 do
+    for col = 0 to cols - 1 do
+      if grid.(row).(col) && count_neighbors ~grid ~rows ~cols (col, row) < 4
+      then ans := !ans + 1
+    done
+  done;
+  !ans
+
 let p2 _input = assert false
 
 let run ~part input =
@@ -12,19 +44,21 @@ let run ~part input =
 let%expect_test "part 1 sample input" =
   let input = In_channel.read_all "../inputs/d4p1.sample" in
   printf "%d" (p1 input);
-  [%expect {| 3 |}]
+  [%expect {| 13 |}]
 
 (*
 let%expect_test "part 2 sample input" =
   let input = In_channel.read_all "../inputs/d4p2.sample" in
   printf "%d" (p2 input);
   [%expect {| 6 |}]
+*)
 
 let%expect_test "part 1" =
   let input = In_channel.read_all "../inputs/d4.in" in
   printf "%d" (p1 input);
-  [%expect {| 1097 |}]
+  [%expect {| 1356 |}]
 
+(*
 let%expect_test "part 2" =
   let input = In_channel.read_all "../inputs/d4.in" in
   printf "%d" (p2 input);
