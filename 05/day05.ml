58a3f5c3456420ccf2c99e3f6988a4812925f3aa
Ethan Zhang <ethanz4@illinois.edu>
2025-12-04 23:12:11 -0600

    part 1

--- 05/day05.ml
+++ 05/day05.ml
@@ -1,6 +1,23 @@
 open! Core
 
-let p1 _input = assert false
+type interval = string * string
+
+let interval_of_string s =
+  let l, r = String.lsplit2_exn s ~on:'-' in
+  (String.pad_left ~char:'0' ~len:15 l, String.pad_left ~char:'0' ~len:15 r)
+
+let parse_db = List.map ~f:interval_of_string
+
+let p1 input =
+  let db_lines, query_lines =
+    String.split_lines input
+    |> List.split_while ~f:(Fn.compose not String.is_empty)
+  in
+  let db = parse_db db_lines in
+  List.count query_lines ~f:(fun query ->
+      let query = String.pad_left ~char:'0' ~len:15 query in
+      List.exists db ~f:(fun (a, b) -> String.(a <= query && query <= b)))
+
 let p2 _input = assert false
 
 let run ~part input =
@@ -12,19 +29,21 @@ let run ~part input =
 let%expect_test "part 1 sample input" =
   let input = In_channel.read_all "../inputs/d5p1.sample" in
   printf "%d" (p1 input);
-  [%expect {| 13 |}]
+  [%expect {| 3 |}]
 
 (*
 let%expect_test "part 2 sample input" =
   let input = In_channel.read_all "../inputs/d5p2.sample" in
   printf "%d" (p2 input);
   [%expect {| 43 |}]
+*)
 
 let%expect_test "part 1" =
   let input = In_channel.read_all "../inputs/d5.in" in
   printf "%d" (p1 input);
-  [%expect {| 1356 |}]
+  [%expect {| 505 |}]
 
+(*
 let%expect_test "part 2" =
   let input = In_channel.read_all "../inputs/d5.in" in
   printf "%d" (p2 input);
