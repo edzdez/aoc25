00e22dc6462425d8c7cf0d5a07ad53a569c43738
Ethan Zhang <ethanz4@illinois.edu>
2025-12-04 23:32:40 -0600

    part 2

--- 05/day05.ml
+++ 05/day05.ml
@@ -1,10 +1,13 @@
 open! Core
 
-type interval = string * string
+type interval = Int64.t * Int64.t
+type extent = Begin of Int64.t | End of Int64.t
+
+let extract_extent = function Begin s -> s | End s -> s
 
 let interval_of_string s =
   let l, r = String.lsplit2_exn s ~on:'-' in
-  (String.pad_left ~char:'0' ~len:15 l, String.pad_left ~char:'0' ~len:15 r)
+  (Int64.of_string l, Int64.of_string r)
 
 let parse_db = List.map ~f:interval_of_string
 
@@ -13,12 +16,33 @@ let p1 input =
     String.split_lines input
     |> List.split_while ~f:(Fn.compose not String.is_empty)
   in
+  let query_lines = List.drop query_lines 1 in
   let db = parse_db db_lines in
-  List.count query_lines ~f:(fun query ->
-      let query = String.pad_left ~char:'0' ~len:15 query in
-      List.exists db ~f:(fun (a, b) -> String.(a <= query && query <= b)))
+  Int64.of_int
+  @@ List.count query_lines ~f:(fun query ->
+      let query = Int64.of_string query in
+      List.exists db ~f:(fun (a, b) -> Int64.(a <= query && query <= b)))
 
-let p2 _input = assert false
+let p2 input =
+  let db =
+    String.split_lines input
+    |> List.take_while ~f:(Fn.compose not String.is_empty)
+    |> parse_db
+    |> List.concat_map ~f:(fun (l, r) -> [ Begin l; End Int64.(r + 1L) ])
+    |> List.sort ~compare:(fun l r ->
+        Int64.compare (extract_extent l) (extract_extent r))
+  in
+  let _, _, cnt =
+    List.fold db ~init:(None, 0, 0L) ~f:(fun (start, nesting, cnt) -> function
+      | Begin l -> (Some (Option.value start ~default:l), nesting + 1, cnt)
+      | End r -> (
+          match start with
+          | None -> assert false
+          | Some start ->
+              if nesting = 1 then (None, 0, Int64.(cnt + r - start))
+              else (Some start, nesting - 1, cnt)))
+  in
+  cnt
 
 let run ~part input =
   match part with
@@ -28,24 +52,20 @@ let run ~part input =
 
 let%expect_test "part 1 sample input" =
   let input = In_channel.read_all "../inputs/d5p1.sample" in
-  printf "%d" (p1 input);
+  printf "%Ld" (p1 input);
   [%expect {| 3 |}]
 
-(*
 let%expect_test "part 2 sample input" =
   let input = In_channel.read_all "../inputs/d5p2.sample" in
-  printf "%d" (p2 input);
-  [%expect {| 43 |}]
-*)
+  printf "%Ld" (p2 input);
+  [%expect {| 14 |}]
 
 let%expect_test "part 1" =
   let input = In_channel.read_all "../inputs/d5.in" in
-  printf "%d" (p1 input);
+  printf "%Ld" (p1 input);
   [%expect {| 505 |}]
 
-(*
 let%expect_test "part 2" =
   let input = In_channel.read_all "../inputs/d5.in" in
-  printf "%d" (p2 input);
-  [%expect {| 8713 |}]
-*)
+  printf "%Ld" (p2 input);
+  [%expect {| 344423158480189 |}]
