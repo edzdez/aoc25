f2ec260623cc3470ef1eb02f8505f2db649c453e
Ethan Zhang <ethanz4@illinois.edu>
2025-12-06 00:32:20 -0600

    cleanup

--- 06/day06.ml
+++ 06/day06.ml
@@ -2,13 +2,6 @@ open! Core
 
 type op = Plus | Times
 
-let parse_nums input =
-  List.map input ~f:(fun line ->
-      String.split_on_chars line ~on:[ ' ' ]
-      |> List.filter_map ~f:(function
-        | "" -> None
-        | s -> Some (int_of_string s)))
-
 let parse_ops input =
   String.split_on_chars input ~on:[ ' ' ]
   |> List.filter_map ~f:(function
@@ -16,7 +9,12 @@ let parse_ops input =
     | "*" -> Some Times
     | _ -> None)
 
-let do_math nums ops =
+let split_nums_and_ops input =
+  let lines = String.split_lines input in
+  let nums, ops = List.split_n lines (List.length lines - 1) in
+  (nums, parse_ops @@ List.hd_exn ops)
+
+let do_math ~ops nums =
   List.zip_exn nums ops
   |> List.fold ~init:0 ~f:(fun acc (nums, op) ->
       acc
@@ -26,58 +24,38 @@ let do_math nums ops =
       | Times -> List.fold nums ~init:1 ~f:( * ))
 
 let p1 input =
-  let lines = String.split_lines input in
-  let nums, ops = List.split_n lines (List.length lines - 1) in
-  let nums, ops =
-    (List.transpose_exn @@ parse_nums nums, parse_ops @@ List.hd_exn ops)
+  let parse_nums input =
+    List.map input ~f:(fun line ->
+        String.split_on_chars line ~on:[ ' ' ]
+        |> List.filter_map ~f:(function
+          | "" -> None
+          | s -> Some (int_of_string s)))
   in
-  do_math nums ops
+  let nums, ops = split_nums_and_ops input in
+  let nums = List.transpose_exn @@ parse_nums nums in
+  do_math ~ops nums
 
 let p2 input =
-  (*
-  let parse_ops input =
-    let rec aux ~acc ~curr = function
-      | [] -> List.drop (List.rev (curr :: acc)) 1
-      | hd :: tl -> (
-          match (hd, curr) with
-          | '+', (op, cnt) -> aux ~acc:((op, cnt - 1) :: acc) ~curr:(Plus, 1) tl
-          | '*', (op, cnt) ->
-              aux ~acc:((op, cnt - 1) :: acc) ~curr:(Times, 1) tl
-          | ' ', (op, cnt) -> aux ~acc ~curr:(op, cnt + 1) tl
-          | _ -> assert false)
-    in
-    aux ~acc:[] ~curr:(Plus, 0) @@ String.to_list input
-  in
-  let parse_nums ~acc
-  let do_math op nums =
-    match op with
-    | Plus -> List.fold nums ~init:0 ~f:( + )
-    | Times -> List.fold nums ~init:1 ~f:( * )
-  in
-  *)
-  let rec parse_nums ~acc ~curr = function
+  let rec parse_nums ?(acc = []) ?(curr = []) = function
     | [] -> List.rev @@ (curr :: acc)
     | hd :: tl -> (
         match int_of_string_opt (String.strip hd) with
         | Some n -> parse_nums ~acc ~curr:(n :: curr) tl
         | None -> parse_nums ~acc:(List.rev curr :: acc) ~curr:[] tl)
   in
-  let lines = String.split_lines input in
-  let nums, ops = List.split_n lines (List.length lines - 1) in
-  let ops = parse_ops @@ List.hd_exn ops in
+  let nums, ops = split_nums_and_ops input in
   let len =
-    Option.value ~default:0
-    @@ List.max_elt ~compare:Int.compare
-    @@ List.map nums ~f:String.length
+    List.map nums ~f:String.length
+    |> List.max_elt ~compare:Int.compare
+    |> Option.value ~default:0
   in
   let nums =
-    nums
-    |> List.map ~f:(Fn.compose String.to_list (String.pad_right ~char:' ' ~len))
+    List.map nums ~f:(Fn.compose String.to_list (String.pad_right ~len))
     |> List.transpose_exn
     |> List.map ~f:String.of_char_list
-    |> parse_nums ~acc:[] ~curr:[]
+    |> parse_nums
   in
-  do_math nums ops
+  do_math ~ops nums
 
 let run ~part input =
   match part with
