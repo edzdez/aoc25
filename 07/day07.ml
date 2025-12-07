c5b1e20a122dbfaabf1099ef68de3732dd4a5529
Ethan Zhang <ethanz4@illinois.edu>
2025-12-07 00:10:03 -0600

    part 2

--- 07/day07.ml
+++ 07/day07.ml
@@ -14,33 +14,44 @@ let parse_diagram input =
     |> String.findi ~f:(fun _ c -> Char.(c = 'S'))
     |> Option.value_exn |> fst
   in
-  (diagram, start)
+  (diagram, Map.of_alist_exn (module Int) [ (start, 1) ])
 
-let simulate ~positions line =
+let simulate ~positions ?(dedup = true) line =
+  let len = Array.length line in
   let cnt = ref 0 in
   let new_ =
-    List.concat_map positions ~f:(fun pos ->
-        if line.(pos) then begin
-          cnt := !cnt + 1;
-          if pos = 0 then [ 1 ]
-          else if pos = Array.length line - 1 then [ pos - 1 ]
-          else [ pos - 1; pos + 1 ]
-        end
-        else [ pos ])
-    |> List.dedup_and_sort ~compare:Int.compare
+    Map.fold positions
+      ~init:(Map.empty (module Int))
+      ~f:(fun ~key ~data acc ->
+        (if line.(key) then begin
+           cnt := !cnt + if dedup then 1 else data;
+           [ key - 1; key + 1 ]
+         end
+         else [ key ])
+        |> List.filter ~f:(fun pos -> pos >= 0 && pos < len)
+        |> List.fold ~init:acc ~f:(fun acc pos ->
+            Map.update acc pos
+              ~f:(Option.value_map ~default:data ~f:(fun v -> v + data))))
   in
   (new_, !cnt)
 
 let p1 input =
   let diagram, start = parse_diagram input in
   let _, cnt =
-    List.fold diagram ~init:([ start ], 0) ~f:(fun (positions, cnt) line ->
+    List.fold diagram ~init:(start, 0) ~f:(fun (positions, cnt) line ->
         let positions, new_ = simulate ~positions line in
         (positions, cnt + new_))
   in
   cnt
 
-let p2 _input = assert false
+let p2 input =
+  let diagram, start = parse_diagram input in
+  let _, cnt =
+    List.fold diagram ~init:(start, 0) ~f:(fun (positions, cnt) line ->
+        let positions, new_ = simulate ~positions ~dedup:false line in
+        (positions, cnt + new_))
+  in
+  cnt + 1
 
 let run ~part input =
   match part with
@@ -53,21 +64,17 @@ let%expect_test "part 1 sample input" =
   printf "%d" (p1 input);
   [%expect {| 21 |}]
 
-(*
 let%expect_test "part 2 sample input" =
   let input = In_channel.read_all "../inputs/d7p2.sample" in
   printf "%d" (p2 input);
-  [%expect {| 3263827 |}]
-*)
+  [%expect {| 40 |}]
 
 let%expect_test "part 1" =
   let input = In_channel.read_all "../inputs/d7.in" in
   printf "%d" (p1 input);
   [%expect {| 1592 |}]
 
-(*
 let%expect_test "part 2" =
   let input = In_channel.read_all "../inputs/d7.in" in
   printf "%d" (p2 input);
-  [%expect {| 11744693538946 |}]
-*)
+  [%expect {| 17921968177009 |}]
