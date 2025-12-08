3991566791080c3cb98b15cb308f12cc4ae43434
Ethan Zhang <ethanz4@illinois.edu>
2025-12-08 00:30:15 -0600

    part 2

--- 08/day08.ml
+++ 08/day08.ml
@@ -1,12 +1,17 @@
 open! Core
 
 module Point = struct
-  type t = int * int * int [@@deriving hash, compare, sexp]
+  type t = int * int * int [@@deriving hash, compare, equal, sexp]
 
   let of_string s =
     match String.split s ~on:',' with
     | [ x; y; z ] -> (int_of_string x, int_of_string y, int_of_string z)
     | _ -> failwith "invalid point"
+
+  let to_string (x, y, z) = sprintf "(%d,%d,%d)" x y z
+
+  let dist_2 (x1, y1, z1) (x2, y2, z2) =
+    ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)) + ((z1 - z2) * (z1 - z2))
 end
 
 let get_pairs = function
@@ -18,35 +23,69 @@ let get_pairs = function
       in
       pairs
 
-let dist_2 (x1, y1, z1) (x2, y2, z2) =
-  ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)) + ((z1 - z2) * (z1 - z2))
+let add_edge ~graph u v =
+  Hashtbl.update graph u
+    ~f:(Option.value_map ~default:[ v ] ~f:(fun acc -> v :: acc));
+  Hashtbl.update graph v
+    ~f:(Option.value_map ~default:[ u ] ~f:(fun acc -> u :: acc))
+
+let create_graph ~edges =
+  let graph = Hashtbl.create (module Point) in
+  List.iter edges ~f:(fun (a, b, _) -> add_edge ~graph a b);
+  graph
+
+let rec dfs ~graph ~seen u =
+  let neighbors = Hashtbl.find_exn graph u in
+  u
+  :: List.concat_map neighbors ~f:(fun v ->
+      if Hash_set.mem seen v then []
+      else begin
+        Hash_set.add seen v;
+        dfs ~graph ~seen v
+      end)
+
+let connected_components graph =
+  let seen = Hash_set.create (module Point) in
+  let vertices = Hashtbl.keys graph in
+  List.fold vertices ~init:[] ~f:(fun acc u ->
+      if not @@ Hash_set.mem seen u then begin
+        Hash_set.add seen u;
+        dfs ~graph ~seen u :: acc
+      end
+      else acc)
 
 let p1 ?(num = 10) input =
   let positions = String.split_lines input |> List.map ~f:Point.of_string in
-  let uf =
-    List.map positions ~f:(fun p -> (p, Union_find.create p))
-    |> Hashtbl.of_alist_exn (module Point)
-  in
-  let pairs =
+  let edges =
     get_pairs positions
-    |> List.sort ~compare:(fun (a, b) (x, y) ->
-        Int.compare (dist_2 a b) (dist_2 x y))
+    |> List.map ~f:(fun (a, b) -> (a, b, Point.dist_2 a b))
+    |> List.sort ~compare:(fun (_, _, x) (_, _, y) -> Int.compare x y)
   in
-  List.take pairs num
-  |> List.iter ~f:(fun (a, b) ->
-      let uf_a = Hashtbl.find_exn uf a in
-      let uf_b = Hashtbl.find_exn uf b in
-      Union_find.union uf_a uf_b);
-  let sizes = Hashtbl.create (module Point) in
-  Hashtbl.iter uf ~f:(fun data ->
-      Hashtbl.update sizes (Union_find.get data)
-        ~f:(Option.value_map ~default:1 ~f:succ));
-  let top_three =
-    Hashtbl.data sizes |> List.sort ~compare:(Fn.flip Int.compare)
+  let graph = create_graph ~edges:(List.take edges num) in
+  let components = connected_components graph in
+  let sizes =
+    List.map components ~f:List.length
+    |> List.sort ~compare:(Fn.flip Int.compare)
   in
-  List.take top_three 3 |> List.fold ~init:1 ~f:( * )
+  List.take sizes 3 |> List.fold ~init:1 ~f:( * )
 
-let p2 _input = assert false
+let p2 input =
+  let positions = String.split_lines input |> List.map ~f:Point.of_string in
+  let num_vertices = List.length positions in
+  let edges =
+    get_pairs positions
+    |> List.map ~f:(fun (a, b) -> (a, b, Point.dist_2 a b))
+    |> List.sort ~compare:(fun (_, _, x) (_, _, y) -> Int.compare x y)
+  in
+  let graph = Hashtbl.create (module Point) in
+  let (x1, _, _), (x2, _, _), _ =
+    List.hd_exn
+    @@ List.drop_while edges ~f:(fun (u, v, _) ->
+        add_edge ~graph u v;
+        List.length (dfs ~graph ~seen:(Hash_set.of_list (module Point) [ u ]) u)
+        <> num_vertices)
+  in
+  x1 * x2
 
 let run ~part input =
   match part with
@@ -59,21 +98,17 @@ let%expect_test "part 1 sample input" =
   printf "%d" (p1 input);
   [%expect {| 40 |}]
 
-(*
 let%expect_test "part 2 sample input" =
   let input = In_channel.read_all "../inputs/d8p2.sample" in
   printf "%d" (p2 input);
-  [%expect {| 40 |}]
-*)
+  [%expect {| 25272 |}]
 
 let%expect_test "part 1" =
   let input = In_channel.read_all "../inputs/d8.in" in
   printf "%d" (p1 ~num:1000 input);
   [%expect {| 57564 |}]
 
-(*
 let%expect_test "part 2" =
   let input = In_channel.read_all "../inputs/d8.in" in
   printf "%d" (p2 input);
-  [%expect {| 17921968177009 |}]
-*)
+  [%expect {| 133296744 |}]
