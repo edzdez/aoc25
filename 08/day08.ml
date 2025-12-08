85306995a0778a3f71cef9603f75bed82a74ab85
Ethan Zhang <ethanz4@illinois.edu>
2025-12-08 12:05:42 -0600

    switch to using heap and union find

--- 08/day08.ml
+++ 08/day08.ml
@@ -14,8 +14,15 @@ module Point = struct
     ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)) + ((z1 - z2) * (z1 - z2))
 end
 
-module Graph = struct
-  type ('a, 'b) t = ('a, 'b) Hashtbl.t
+module Graph : sig
+  type 'a t
+
+  val create : ?edges:('a * 'a) list -> 'a Hashtbl_intf.Hashtbl.Key.t -> 'a t
+
+  val connected_components :
+    'a Hashtbl_intf.Hashtbl.Key.t -> graph:'a t -> 'a list list
+end = struct
+  type 'a t = ('a, 'a list) Hashtbl.t
 
   let add_edge ~graph u v =
     Hashtbl.update graph u
@@ -25,7 +32,7 @@ module Graph = struct
 
   let create ?(edges = []) m =
     let graph = Hashtbl.create m in
-    List.iter edges ~f:(fun (a, b, _) -> add_edge ~graph a b);
+    List.iter edges ~f:(fun (a, b) -> add_edge ~graph a b);
     graph
 
   let dfs ~graph ~seen u =
@@ -53,20 +60,59 @@ module Graph = struct
         else acc)
 end
 
+module Union_find : sig
+  type 'a t
+
+  val create : 'a Hashtbl_intf.Hashtbl.Key.t -> 'a t
+  val size : 'a t -> 'a -> int
+  val union : 'a t -> 'a -> 'a -> unit
+end = struct
+  type 'a t = {
+    classes : ('a, 'a Union_find.t) Hashtbl.t;
+    sizes : ('a, int) Hashtbl.t;
+  }
+
+  let create m = { classes = Hashtbl.create m; sizes = Hashtbl.create m }
+
+  let size { classes; sizes } u =
+    match Hashtbl.find classes u with
+    | None -> begin
+        Hashtbl.add_exn classes ~key:u ~data:(Union_find.create u);
+        Hashtbl.add_exn sizes ~key:u ~data:1;
+        1
+      end
+    | Some c -> Hashtbl.find_exn sizes (Union_find.get c)
+
+  let union ({ classes; sizes } as t) u v =
+    let size_u = size t u in
+    let size_v = size t v in
+    let class_u = Hashtbl.find_exn classes u in
+    let class_v = Hashtbl.find_exn classes v in
+    if not @@ Union_find.same_class class_u class_v then begin
+      Union_find.union class_u class_v;
+      Hashtbl.set sizes ~key:(Union_find.get class_u) ~data:(size_u + size_v)
+    end
+end
+
 let rec get_pairs ?(seen = []) ?(acc = []) = function
   | [] -> acc
   | hd :: tl ->
       let acc = List.fold seen ~init:acc ~f:(fun acc y -> (y, hd) :: acc) in
       get_pairs ~seen:(hd :: seen) ~acc tl
 
-let p1 ?(num = 10) input =
+let p1 ?(num = 1000) input =
   let positions = String.split_lines input |> List.map ~f:Point.of_string in
   let edges =
     get_pairs positions
     |> List.map ~f:(fun (a, b) -> (a, b, Point.dist_2 a b))
-    |> List.sort ~compare:(fun (_, _, x) (_, _, y) -> Int.compare x y)
+    |> Pairing_heap.of_list ~cmp:(fun (_, _, x) (_, _, y) -> Int.compare x y)
   in
-  let graph = Graph.create (module Point) ~edges:(List.take edges num) in
+  let edges =
+    List.init num ~f:(fun _ ->
+        let u, v, _ = Pairing_heap.pop_exn edges in
+        (u, v))
+  in
+  let graph = Graph.create (module Point) ~edges in
   let components = Graph.connected_components (module Point) ~graph in
   let sizes =
     List.map components ~f:List.length
@@ -80,17 +126,14 @@ let p2 input =
   let edges =
     get_pairs positions
     |> List.map ~f:(fun (a, b) -> (a, b, Point.dist_2 a b))
-    |> List.sort ~compare:(fun (_, _, x) (_, _, y) -> Int.compare x y)
+    |> Pairing_heap.of_list ~cmp:(fun (_, _, x) (_, _, y) -> Int.compare x y)
   in
-  let graph = Graph.create (module Point) in
-  let (x1, _, _), (x2, _, _), _ =
-    List.hd_exn
-    @@ List.drop_while edges ~f:(fun (u, v, _) ->
-        Graph.add_edge ~graph u v;
-        List.length
-          (Graph.dfs ~graph ~seen:(Hash_set.of_list (module Point) [ u ]) u)
-        <> num_vertices)
-  in
+  let uf = Union_find.create (module Point) in
+  ignore
+  @@ Pairing_heap.pop_while edges (fun (u, v, _) ->
+      Union_find.union uf u v;
+      Union_find.size uf u <> num_vertices);
+  let (x1, _, _), (x2, _, _), _ = Pairing_heap.top_exn edges in
   x1 * x2
 
 let run ~part input =
@@ -101,7 +144,7 @@ let run ~part input =
 
 let%expect_test "part 1 sample input" =
   let input = In_channel.read_all "../inputs/d8p1.sample" in
-  printf "%d" (p1 input);
+  printf "%d" (p1 ~num:10 input);
   [%expect {| 40 |}]
 
 let%expect_test "part 2 sample input" =
